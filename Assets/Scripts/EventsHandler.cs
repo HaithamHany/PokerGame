f2d9b1be395b5f230b711c8744a5caf117f5a7a8
Haitham Abdel-Salam <haithamhany7@gmail.com>
2022-09-10 08:05:36 -0400

    Updating events handler

--- Assets/Scripts/EventsHandler.cs
+++ Assets/Scripts/EventsHandler.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 
 public class EventsHandler : MonoBehaviour
 {
-    public static EventsHandler Instance;
+    private static EventsHandler _instance;
+    public static EventsHandler Instance => _instance;
+    
+    //Events
+    public event Action<int> OnActivatingObject;
+    public event Action<int> OnDeactivatingObject;
+    public event Action<EPlayer, float> OnPlayerPlacedBet;
+    public event Action OnAllBetsPlaced;
+    public event Action<EPlayer> OnPlayerTurn;
+    public event Action<ERoundResult> OnRoundDone;
+
 
     private void Awake()
     {
-        if (Instance != null && Instance != this)
+        if (_instance != null && _instance != this)
         {
             Destroy(gameObject);
         }
         else
         {
             DontDestroyOnLoad(gameObject);
-            Instance = this;
+            _instance = this;
         }
     }
-
-    public event Action<int> OnActivatingObject;
-    public event Action<int> OnDeactivatingObject;
-
+    
     public void ActivatingObject(int objId)
     {
         if(OnActivatingObject != null)
@@ -37,4 +44,48 @@ public class EventsHandler : MonoBehaviour
             OnDeactivatingObject(objId);
         }
     }
+    
+    public void DeactivatingObject(EPlayer player, float amount)
+    {
+        if (OnPlayerPlacedBet != null)
+        {
+            OnPlayerPlacedBet(player, amount);
+        }
+    }
+    
+    public void AllBetsPlaced()
+    {
+        if (OnAllBetsPlaced != null)
+        {
+            OnAllBetsPlaced();
+        }
+    }
+    
+    public void PlayerTurn(EPlayer player)
+    {
+        if (OnPlayerTurn != null)
+        {
+            OnPlayerTurn(player);
+        }
+    }
+    
+    public void RoundResult(ERoundResult result)
+    {
+        if (OnRoundDone != null)
+        {
+            OnRoundDone(result);
+        }
+    }
+}
+
+public enum EPlayer
+{
+    Player01 = 0,
+    Player02 = 1
+}
+
+public enum ERoundResult
+{
+    Win = 0,
+    Lose = 1,
 }
